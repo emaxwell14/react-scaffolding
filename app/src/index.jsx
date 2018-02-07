4d8c4b755f7ab04539c9fa4cabda443400b19429
Eoghan Maxwell <eoghan.maxwell@cegedim-activ.com>
2018-02-07 12:02:14 +0100

    Adding history with connected router to allow refreshing

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
+import createHistory from 'history/createHashHistory';
 import Main from './Main/Main';
 import Header from './Header/Header';
 import store from './store';
@@ -12,13 +13,13 @@ const App = () => (
         <Main />
     </div>
 );
-
+const history = createHistory();
 render(
     (
         <Provider store={store}>
-            <BrowserRouter>
+            <ConnectedRouter history={history}>
                 <App />
-            </BrowserRouter>
+            </ConnectedRouter>
         </Provider>
     ),
     document.getElementById('app'),
